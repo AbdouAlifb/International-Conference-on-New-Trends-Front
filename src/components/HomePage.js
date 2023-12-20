7ce8a2f40d5522c376c28c04efd796eb6e874183
ALI ABDOU <ali.abdoufb@gmail.com>
2023-12-20 22:08:12 +0100

    Your commit message here

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -31,10 +31,41 @@ const HomePage = () => {
                     </p>
                 </div>
             </div>
-            <div className="mt-4">
+            {/* About Section */}
+            <div className="container mt-4">
+                <section>
+                    <h2>About the Conference</h2>
+                    {/* <img src={`${process.env.PUBLIC_URL}/data.jpg`} alt="About Conference" style={{ width: '100%', height: '200px', marginBottom: '20px' }} /> */}
+                    <p>
+                        
+The 1st International Conference on Data Science and Artificial Intelligence (DS&AI) will take place on 27-29 November 2023, organized by the Department of Information & Communications Technologies of the Asian Institute of Technology, Thailand. The conference’s venue will be at the Berkeley Hotel Pratunam, Bangkok.
+
+The main goal of the DS&AI conference series is to contribute to the growth of data science and artificial intelligence knowledge in developing countries in the South-East Asia region. DS&AI provide a platform for researchers and practitioners from developed and these developing countries to share their knowledge and expertise on data science and artificial intelligence and discuss the latest development in the field, so to build capacity and improve the understanding of data science and AI in the South-East Asia region. The DS&AI conference brings together researchers from different countries providing an opportunity to foster the development of partnerships and collaboration. By raising awareness and interest directly in the local community, the DS&AI conference aims to stimulate and to help the development of data science and artificial intelligence research and application in these countries.
+                    </p>
+                </section>
+
+                {/* Keynote Speakers Section */}
+                <section>
+                    <h2>Keynote Speakers</h2>
+                    <div style={{ display: 'flex', justifyContent: 'space-around', marginBottom: '20px' }}>
+                        {/* Replace with actual images and details of the speakers */}
+                        <img src={`${process.env.PUBLIC_URL}/s1.PNG`} alt="Speaker 1" style={{ width: '15%', height: 'auto' }} />
+                        <img src={`${process.env.PUBLIC_URL}/s2.PNG`} alt="Speaker 2" style={{ width: '15%', height: 'auto' }} />
+                        <img src={`${process.env.PUBLIC_URL}/s3.PNG`} alt="Speaker 3" style={{ width: '15%', height: 'auto' }} />
+                    </div>
+                    <p>Meet our distinguished keynote speakers who are leading experts in the field of data science and AI.</p>
+                </section>
+
+                {/* Program Section */}
+                <section>
+                    <h2>Conference Program</h2>
+                    <img src={`${process.env.PUBLIC_URL}/post.PNG`} alt="Conference Program" style={{ width: '50%', height: 'auto', marginBottom: '20px' }} />
+                    <p>Check out the detailed conference program for information on sessions, talks, and events.</p>
+                </section>
+
+                {/* News Section */}
                 <NewsSection />
             </div>
-          
         </div>
     );
 };
